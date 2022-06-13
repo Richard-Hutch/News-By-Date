20d704ad79502d543dbe3083835bf1f623171251
Richard Hutcheson <richardhutch80@gmail.com>
2022-06-13 13:45:18 -0500

    added picsum api call

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, Injectable, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import {HttpServiceService} from 'src/app/http-service.service'
-import { FormControl } from '@angular/forms';
+
 type Article={
   headline: string;
   url: string;
@@ -14,14 +15,15 @@ type Article={
 export class HomePageComponent implements OnInit {
 
   selectedDate! : any;
+  randPic!: any;
   articles: Array<Article> = [];
-  constructor(private httpService: HttpServiceService) {}
+  constructor(private httpService: HttpServiceService, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {}
 
   //TO-DO: add http error handling and styling
 
-  onSearch(dateForm : any){
+  onSearchForArticles(dateForm : any){
     this.articles.length = 0;
     this.httpService.getArticles(this.selectedDate).subscribe({
       next: (response)=> {
@@ -38,5 +40,20 @@ export class HomePageComponent implements OnInit {
       error: (e)=> console.log(e),
       complete: () => console.log("complete")
     });
+  }
+
+  onSearchForRandPic(){
+
+    this.httpService.getRandPic().subscribe({
+      next: (response) =>{
+        console.log(response);
+        this.randPic = URL.createObjectURL(response); 
+        this.randPic = this.sanitizer.bypassSecurityTrustResourceUrl(this.randPic);
+      },
+      error: (e) =>{
+        console.error("Error encountered searching for random picture:");
+        console.error(e);
+      }
+    })
   }
 }
--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -2,28 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   //retrieve articles from particular date
   getArticles(date: string): Observable<any>{
     if (!this.inCorrectDateFormat(date)){
       console.error("date '" + date + "' in getArticles does not match format");
       throw new Error("invalid date format");
     }
-    const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${date}&end_date=${date}&sort=relevance&api-key=${environment.NYT_API_KEY}`
-    const OPTIONS = {
-      responseType: 'json',
-    }
+    const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${date}&end_date=${date}&sort=relevance&api-key=${environment.NYT_API_KEY}`;
     return this.http.get(URL);
   }
   //returns if date passes regexp, true if yes, else false
   inCorrectDateFormat(date: string): boolean{
       return new RegExp('^[0-9]{4}-[0-9]{2}-[0-9]{2}$').test(date);
+  }
+  //retrieve random picture from Lorem Picsum
+  getRandPic(){
+    const URL = "https://picsum.photos/400";
+    return this.http.get(URL, {responseType: "blob"});
   }
   
 }
